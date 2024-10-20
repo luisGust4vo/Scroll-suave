2c623e3a387fa58cdde6db57e08e67426ef9fc84
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2024-10-20 18:59:11 -0300

    Add files via upload

---
+++ scroll-suave-quebra/scroll.js
@@ -0,0 +1,4 @@
+// Força a adição de um scroll vertical na página
+document.body.style.overflowY = "scroll";
+// Caso precise de um scroll horizontal, use a linha abaixo:
+// document.body.style.overflowX = 'scroll';
